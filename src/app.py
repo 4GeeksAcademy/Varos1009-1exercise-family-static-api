4f63d3b0acc2fb6bbe0157047547b15daa2f1927
Varos1009 <85431234+Varos1009@users.noreply.github.com>
2024-05-18 12:30:38 +0000

    Family-API

--- src/app.py
+++ src/app.py
@@ -25,18 +25,53 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/members', methods=['POST'])
+def handle_add_member():
+     body = request.get_json()  # Get the request body content
+     if body is None:
+        return "The request body is null", 400
+     if 'first_name' not in body:
+        return 'You need to specify the first_name', 400
+     if 'age' not in body:
+        return 'You need to specify the age', 400
+     if 'lucky_numbers' not in body:
+        return 'You need to specify the lucky_numbers', 400
+     add_member=jackson_family.add_member(body)
+    
+     return jsonify(add_member), 200
+     
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members/<int:id_member>',methods=['DELETE'])
+def handle_delete_member(id_member):
+    member=jackson_family.delete_member(id_member)
+
+    if member is None:
+       return jsonify({'err':'member not found'}), 404
+    
+    
+
+    return jsonify(member), 200
+
+@app.route('/members/<int:id_member>',methods=['GET'])
+def handle_get_member(id_member):
+    member=jackson_family.get_member(id_member)
+
+    if member is None:
+       return jsonify({'err':'member not found'}), 404
+    
+    response=member
+
+    return jsonify(response), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
